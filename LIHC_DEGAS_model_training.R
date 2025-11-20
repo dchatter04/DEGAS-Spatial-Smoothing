8a3bf1552669da96e91c49a6e4d259b6ef37cb45
dchatter04 <debolinastat@gmail.com>
2025-11-20 01:43:16 -0600

    Update and rename LIHC_DEGAS_predictions.R to LIHC_DEGAS_model_training.R

--- LIHC_DEGAS_model_training.R
+++ LIHC_DEGAS_model_training.R
@@ -1,4 +1,8 @@
 ##### Do this after you run the bulkRNAseq data cleanup code and save the patDat.csv and patLab.csv files
+
+#r/4.4.1
+# python/3.10.10
+
 library(devtools)
 library(withr)
 library(rprojroot)
@@ -7,21 +11,22 @@ library(magrittr)
 library(here)
 # If DEGAS hasn't been installed, do so using this command (after creating a r-packages file in your root directory)
 #with_libpaths(new = '~/r-packages', install_github("tsteelejohnson91/DEGAS"))
-library(DEGAS)
+#library(DEGAS)
+library(DEGAS, lib.loc = "~/DEGAS_package") #this is the folder that has DEGAS installed
 
 set.seed(2)
 
 meta_data_columns <- c("V1", "cell_id", "slide_ID_numeric", "Run_Tissue_name", "x_FOV_px", "y_FOV_px", "qcFlagsFOV", "cellType", "niche")
 
 # What is the name of this experiment/sample?
-sample_name <- "liver_coxmyx"
+sample_name <- "lihc_200genes"
 
 # Patient data
 ## RNA-seq
-patDat <- data.table::fread('~/LIHC_patDat.csv', sep = ",") # Tumor and normal tissue
+patDat <- data.table::fread('~/patDat.csv', sep = ",") # Tumor and normal tissue
 
 ## Clinical outcomes
-patLab <- data.table::fread('~LIHC_patLab.csv', sep = ",")
+patLab <- data.table::fread('~/patLab.csv', sep = ",")
 
 # Need non NA follow up time and status
 patLab <- patLab %>% filter(!is.na(OS_time))
@@ -31,12 +36,9 @@ patDat <- patDat %>% inner_join(patLab)
 
 ################## # ST data (CosMx)########
 #this is the metadata associated with the CosMx data obtained from https://nanostring.com/products/cosmx-spatial-molecular-imager/ffpe-dataset/human-liver-rna-ffpe-dataset/###
-
 meta_df <- read_csv('~/cosmx_liver_metadata.csv') # To get the coordinates
 
-#### refer to the LIHC_metadata_preprocessing to obtain the normalized gene expression data for the CosMx LIHC samples "cosmx_liver_data.csv"
-
-st_data <- data.table::fread('/N/project/degas_st/cosmyx/data/processed/liver/cosmyx_liver_data.csv')
+st_data <- data.table::fread('~/cosmyx_liver_data.csv')
 
 st_data <- st_data %>%
   mutate(FOV = str_replace(str_replace(cell_id, "c_[:digit:]_", ""), "_[:digit:].*$", ""), .before = cell_id)
@@ -64,26 +66,17 @@ intersecting_genes <-
 genes <- patDat %>% select(all_of(intersecting_genes)) %>% apply(2, var) %>% sort(decreasing=TRUE) %>% names() %>% head(200)
 
 
-# Initialize function to run DEGAS iteratively across an increasing number of feature sets.
-runDEGASBlankCox <- function(stDat, patDat, patLab, genes, sample_name, iter) {
+# Initialize function to run DEGAS iteratively across increasing number of feature sets.
+runDEGASBlankCox <- function(stDat, patDat, patLab, genes, iter) {
   
   path.data = '/N/project/degas_st/cosmyx/data/DEGAS/'
-  path.result = '/N/project/degas_st/cosmyx/liver_itcr/'
+  path.result = '/N/project/degas_st/cosmyx/lihc_output_Nov14/'
   initDEGAS()
-  DEGAS.toolsPath <<- paste0("/N/project/degas_st/DEGAS/tools/")
-  # overwrite the DEGAS path with your python location (module load deeplearning/2.9.1)
+  DEGAS.toolsPath <<- '/N/project/degas_st/DEGAS_package/DEGAS/tools/'
+  # overwrite the DEGAS path with your python location
   DEGAS.pyloc <<- '/N/soft/rhel8/deeplearning/Python-3.10.10/bin/python3.10'
   tmpDir = paste0(path.result, 'tmp/')
-  #DEGAS.seed <<- 100
-
-  #/N/soft/rhel8/python/gnu/3.10.5/lib/python3.10/site-packages/tensorflow/__init__.py
-  #/N/soft/rhel8/deeplearning/Python-3.10.10/bin/python3.10
-  #/geode2/soft/hps/rhel7/deeplearning/Python-3.10.5/bin/python3.10
-  #path.data = ''
-  #path.result = ''
-  #initDEGAS()
-  #tmpDir = paste0(path.result, 'tmp/')
-
+ 
   set_seed_term(2)
   ccoxModel_PRAD =
     runCCMTLBag(
@@ -94,13 +87,14 @@ runDEGASBlankCox <- function(stDat, patDat, patLab, genes, sample_name, iter) {
       tmpDir = tmpDir,
       'BlankCox','DenseNet',3,5)
 
-  saveRDS(ccoxModel_PRAD, file = paste0('/N/project/degas_st/cosmyx/liver_itcr/', sample_name, "_", iter, "_",  Sys.Date(), '.RDS'))
+  saveRDS(ccoxModel_PRAD, file = paste0('/N/project/degas_st/cosmyx/lihc_output_Nov14/',iter, "_",  Sys.Date(), '.RDS'))
 
   model_output <-
     tibble(
       as_tibble(predClassBag(ccoxModel_PRAD,
                              stDat %>% select(all_of(genes)),
                              'pat')),
+      cell_id = stDat$cell_id,
       fov = stDat$FOV,
       coord1 = stDat$x_slide_mm,
       coord2 = stDat$y_slide_mm,
@@ -116,17 +110,18 @@ runDEGASBlankCox <- function(stDat, patDat, patLab, genes, sample_name, iter) {
 }
 
 
-for (i in c(10, 20, 40, 80, 120, 160, 200)) {
+i=length(genes)
+
   output <-
     runDEGASBlankCox(stDat = st_data,
                      patDat = patDat,
                      patLab = patLab,
                      genes = genes[1:i],
-                     sample_name = sample_name,
                      iter = i)
   iter = i
 
   med <- output %>% pull(Hazard) %>% median
 
-  write_csv(output, file = paste0('/N/project/degas_st/cosmyx/liver_itcr/', sample_name, "_", iter, "_",  Sys.Date(), '.csv'))
-}
+  write_csv(output, file = paste0('/N/project/degas_st/cosmyx/lihc_output_Nov14/',iter, "_",  Sys.Date(), '.csv'))
+
+
