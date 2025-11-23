d7f1651a27706571e5f9b52488401a81e10f2607
dchatter04 <debolinastat@gmail.com>
2025-11-23 01:35:10 -0500

    Update CosMx _LIHC_preprocessing.R

--- LIHC CosMx/CosMx _LIHC_preprocessing.R
+++ LIHC CosMx/CosMx _LIHC_preprocessing.R
@@ -1,6 +1,6 @@
 library(tidyverse)
 
-st_data <- data.table::fread('/N/project/degas_st/cosmyx/Cosmx_liver_data.csv')
+st_data <- data.table::fread('/N/project/degas_st/cosmyx/Cosmx_liver_metadata.csv')
 
 meta_data_columns <- c("V1", "cell_id", "slide_ID_numeric", "Run_Tissue_name", "x_FOV_px", "y_FOV_px", "qcFlagsFOV", "cellType", "niche")
 
@@ -13,4 +13,4 @@ st_data_out <- cbind(
 )
 colnames(st_data_out)[(length(meta_data_columns)+1): length(st_data_out)] <- colnames(rna_df)
 
-data.table::fwrite(st_data_out, '/N/project/degas_st/cosmyx/data/processed/liver/cosmyx_liver_data.csv')
+data.table::fwrite(st_data_out, '/N/project/degas_st/cosmyx/data/processed/liver/cosmyx_liver_countdata.csv')
